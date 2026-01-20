b5941b5e292f0bafe7b6642a14fd537284d7c6cf
jheader <753052378@qq.com>
2026-01-20 13:30:37 +0800

    model

--- model/user.go
+++ model/user.go
@@ -1,1 +1,17 @@
 package model
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint64         `json:"id" gorm:"primaryKey"`
+	Username  string         `json:"username" gorm:"uniqueIndex; not null; size:50"`
+	Password  string         `json:"password,omitempty" gorm:"not null"`
+	Email     string         `json:"email" gorm:"uniqueIndex;not null;size:255"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
